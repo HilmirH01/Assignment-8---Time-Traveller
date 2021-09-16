8ad1c439a36291ba4b8ecf7437f7a3b7597db7a0
Guðmundur Sigurðsson <gudmundurss21@ru.is>
2021-09-16 13:46:21 +0000

    HJALP

--- Time-Traveller.py
+++ Time-Traveller.py
@@ -1,11 +1,17 @@
-x = 1
-y = 1
+def x(x):
+    return x+1
+def ussilon(y):
+    return y+1
 
-TRAVEL_STR = ("You can travel:")
+TRAVEL_STR = "You can travel: "
 DIRECTION_N = "(N)orth"
 DIRECTION_S = "(S)outh"
 DIRECTION_E = "(E)ast"
 DIRECTION_W = "(W)est"
+
+def n(y):
+    return ussilon(y)
+
 
 tile_1_1_dir = (TRAVEL_STR + DIRECTION_N)
 tile_1_2_dir = (TRAVEL_STR + DIRECTION_N + " or " + DIRECTION_E + " or " + DIRECTION_S)
@@ -18,33 +24,16 @@ while x != 3 or y != 1:
         print(tile_1_1_dir + ".")
         dir_input
         if dir_input == "n" or dir_input == "N":
-            y += 1
+            n(1)
+            print(x,n)
+            dir_input = input("Direction: ")
         else:
             print("Not a valid direction!")
-    
-    if x == 1 and y == 2:
+    elif x == 1 and y == 2:
         print(tile_1_2_dir)
         dir_input
         if dir_input == "n" or dir_input == "N":
-            y += 1
+            n(1)
+            print(x,y)
         elif dir_input == "e" or dir_input == "N":
-            
-         
-            
-    
-
-    
-
-
-    
-
-
-
-
-
-
-    
-
-
-
-
+            y -= 1
