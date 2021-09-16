d8d9716279ea2a53d4cf2c373cc77c552e7b12f6
Hilmir Hrafnsson <hilmir.hrafnsson@gmail.com>
2021-09-16 13:46:58 +0000

    for gummi to pull

--- Time-Traveller.py
+++ Time-Traveller.py
@@ -1,34 +1,74 @@
 x = 1
 y = 1
 
-TRAVEL_STR = ("You can travel:")
+#Fastar fyrir strengi
+TRAVEL_STR = "You can travel:"
 DIRECTION_N = "(N)orth"
 DIRECTION_S = "(S)outh"
 DIRECTION_E = "(E)ast"
 DIRECTION_W = "(W)est"
+ERROR_DIR = "Not a valid direction!" #For when the user inputs an impossible direction.
 
+#Breytur fyrir áttir
 tile_1_1_dir = (TRAVEL_STR + DIRECTION_N)
 tile_1_2_dir = (TRAVEL_STR + DIRECTION_N + " or " + DIRECTION_E + " or " + DIRECTION_S)
-destination_e = (TRAVEL_STR + DIRECTION_E)
-destination_w = (TRAVEL_STR + DIRECTION_W)
-dir_input = input("Direction: ")
+tile_1_3_dir = (TRAVEL_STR + DIRECTION_E + " or " + DIRECTION_S)
+tile_2_1_dir = (TRAVEL_STR + DIRECTION_N)
+tile_2_2_dir = (TRAVEL_STR + DIRECTION_S + " or " + DIRECTION_W)
+tile_2_3_dir = (TRAVEL_STR + DIRECTION_W + " or " + DIRECTION_E)
+tile_3_3_dir = (TRAVEL_STR + DIRECTION_W + " or " + DIRECTION_S)
+tile_3_2_dir = (TRAVEL_STR + DIRECTION_N + " or " + DIRECTION_S)
+winning_tile_3_1 = "Victory!"
+
 
 while x != 3 or y != 1:
+#Starting tile 1,1    
     if x == 1 and y == 1:
         print(tile_1_1_dir + ".")
-        dir_input
+        dir_input = input("Direction: ")
         if dir_input == "n" or dir_input == "N":
             y += 1
         else:
-            print("Not a valid direction!")
-    
+            print(ERROR_DIR)
+
+#When entering tile 1,2    
     if x == 1 and y == 2:
         print(tile_1_2_dir)
-        dir_input
+        dir_input = input("Direction: ")
         if dir_input == "n" or dir_input == "N":
             y += 1
-        elif dir_input == "e" or dir_input == "N":
-            
+        elif dir_input == "e" or dir_input == "E":
+            x += 1 
+        elif dir_input == "s" or dir_input == "S":
+            y -= 1
+        else:
+            print(ERROR_DIR)
+
+#When entering tile 1,3
+    if x == 1 and y == 3:
+        print(tile_1_3_dir)
+        dir_input = input("Direction: ")
+        if dir_input == "s" or dir_input == "S":
+            y -= 1
+        elif dir_input == "e" or dir_input == "E":
+            x += 1
+        else:
+            print(ERROR_DIR)
+
+#When entering tile 2,1
+    if x == 2 and y == 1:
+        print(tile_2_1_dir + ".")
+        dir_input = input("Direction: ")
+        if dir_input == "n" or dir_input == "N":
+            y += 1
+        else:
+            print(ERROR_DIR)
+                
+        
+
+    
+
+
          
             
     
